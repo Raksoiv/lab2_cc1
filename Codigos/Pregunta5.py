2c8a3617d2c9f7060065e135efad454d9e8410cd
Oscar Arturo Rencoret Muñoz <oscar.rencoret@lds-pc45.inf.santiago.usm.cl>
2015-05-04 15:06:53 -0400

    Pregunta 5

--- Codigos/Pregunta5.py
+++ Codigos/Pregunta5.py
@@ -1,4 +1,20 @@
 from math import *
+import math
 
-def taylor(x, x0):
-	
+e_mach = 2**(-52)
+
+def taylor_a(x, x0):
+	def g(x, x0, c):
+		return cos(x0) - sin(x0) * (x - x0) - cos(c) * ((x - x0) / 2.0)
+	c = math.pi
+	while(g(x, x0, c) - cos(x) > e_mach):
+		c = g(x, x0, c)
+	print("c = " + str(c))
+
+def taylor_b():
+	i = -.75
+	while(i < .85):
+		taylor(i, math.pi/2.0)
+		i += .5
+
+taylor(math.pi, math.pi/2.0)
